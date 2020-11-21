0ff306975fda8d36ef52db126fc84ba2452f66c3
3541 <alex@emobrien.com>
2020-11-21 16:32:04 +1100

    Add logging and misc util.

---
+++ src/log.c
@@ -0,0 +1,30 @@
+#include <a3/log.h>
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <a3/util.h>
+
+static FILE* log_out = NULL;
+static LogLevel log_level = ERROR;
+
+void log_init(FILE* out, LogLevel level) { log_out = out; log_level = level; }
+
+FORMAT_FN(2, 3)
+void log_fmt(LogLevel level, const char* fmt, ...) {
+    if (level < log_level)
+        return;
+
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(log_out, fmt, args);
+    fputc('\n', log_out);
+    va_end(args);
+}
+
+void log_msg(LogLevel level, const char* msg) { log_fmt(level, "%s", msg); }
+
+void log_error(int error, const char* msg) {
+    log_fmt(ERROR, "Error: %s (%s).", strerror(error), msg);
+}
