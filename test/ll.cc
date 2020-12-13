5ff2eae60626bbcbbe6388473b87633bfd13cc72
3541 <alex@emobrien.com>
2020-12-13 19:03:03 +1100

    Partway on linked list tests.

---
+++ test/ll.cc
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+
+#include <a3/ll.h>
+
+LL_DEFINE_STRUCTS(TestStruct);
+
+struct TestStruct {
+    size_t data;
+    LL_NODE(TestStruct) {};
+
+    explicit TestStruct(size_t d) : data { d } {}
+};
+
+LL_DECLARE_METHODS(TestStruct);
+LL_DEFINE_METHODS(TestStruct);
+
+class LLTest : public ::testing::Test {
+protected:
+    LL(TestStruct) list {};
+
+    void SetUp() override {
+        LL_INIT(TestStruct)(&list);
+    }
+};
+
+TEST_F(LLTest, init) {
+    EXPECT_EQ(list.head.next, &list.end);
+    EXPECT_EQ(&list.head, list.end.prev);
+}
+
+TEST_F(LLTest, enqueue_dequeue) {
+    auto t = new TestStruct { 1234 };
+    LL_ENQUEUE(TestStruct)(&list, t);
+
+    auto p = LL_PEEK(TestStruct)(&list);
+    EXPECT_EQ(p->data, 1234);
+    EXPECT_EQ(p->_ll_ptr.prev, &list.head);
+    EXPECT_EQ(p->_ll_ptr.next, &list.end);
+    EXPECT_EQ(list.head.next, &p->_ll_ptr);
+    EXPECT_EQ(list.end.prev, &p->_ll_ptr);
+    EXPECT_EQ(p, t);
+
+    p = LL_DEQUEUE(TestStruct)(&list);
+    EXPECT_EQ(p, t);
+    EXPECT_EQ(list.head.next, &list.end);
+    EXPECT_EQ(&list.head, list.end.prev);
+
+    delete p;
+}
+
+TEST_F(LLTest, many_insertions) {
+    for (size_t i = 0; i < 128; i++) {
+        LL_ENQUEUE(TestStruct)(&list, new TestStruct { i });
+    }
+}
