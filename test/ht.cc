91312fe8fe458140734d95d8a94d20fc2c54dcc2
3541 <3541ax@gmail.com>
2020-12-19 12:53:48 +1100

    HT: Add more extensive tests.

--- test/ht.cc
+++ test/ht.cc
@@ -1,7 +1,11 @@
+#include <vector>
+
 #include <gtest/gtest.h>
 
 #include <a3/ht.h>
 #include <a3/str.h>
+
+using std::vector;
 
 HT_DEFINE_STRUCTS(CString, CString)
 
@@ -35,4 +39,38 @@ TEST_F(HTTest, insert_and_delete) {
     EXPECT_TRUE(HT_DELETE(CString, CString)(&table, CS("A key")));
     EXPECT_EQ(HT_SIZE(CString, CString)(&table), 0);
     EXPECT_FALSE(HT_FIND(CString, CString)(&table, CS("A key")));
+}
+
+TEST_F(HTTest, grow) {
+    vector<CString> keys;
+
+    auto all_present = [&]() {
+        for (auto& key : keys) {
+            auto* value = HT_FIND(CString, CString)(&table, key);
+            EXPECT_TRUE(value);
+            EXPECT_EQ(string_cmp(key, *value), 0);
+        }
+    };
+
+    for (size_t i = 0; i < HT_INITIAL_CAP * 4; i++) {
+        CString s = S_CONST(string_itoa(i));
+        HT_INSERT(CString, CString)(&table, s, s);
+        keys.push_back(s);
+    }
+
+    EXPECT_EQ(HT_SIZE(CString, CString)(&table), HT_INITIAL_CAP * 4);
+    EXPECT_GE(table.cap, HT_INITIAL_CAP * 4);
+    all_present();
+
+    while (keys.size()) {
+        auto key = keys.begin() + random() % (ssize_t)keys.size();
+        HT_DELETE(CString, CString)(&table, *key);
+        String tmp = CS_MUT(*key);
+        string_free(&tmp);
+        keys.erase(key);
+
+        all_present();
+    }
+
+    EXPECT_EQ(HT_SIZE(CString, CString)(&table), 0);
 }
