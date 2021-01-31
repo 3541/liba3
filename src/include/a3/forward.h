a00d39ed6506a36d181818dc35365a347d3a2b90
3541 <alex@emobrien.com>
2021-01-31 15:15:41 +1100

    MISC: Add forwarding header.

---
+++ src/include/a3/forward.h
@@ -0,0 +1,16 @@
+/*
+ * FORWARD -- Type forward declarations.
+ *
+ * Copyright (c) 2021, Alex O'Brien <3541ax@gmail.com>
+ *
+ * This file is licensed under the BSD 3-clause license. See the LICENSE file in
+ * the project root for details.
+ */
+
+#pragma once
+
+struct A3Buffer;
+typedef struct A3Buffer A3Buffer;
+
+struct A3Pool;
+typedef struct A3Pool A3Pool;
