80f47b3ba3a8c7687016b4cdf34a771098330b1e
3541 <alex@emobrien.com>
2021-06-09 11:37:07 +1000

    TIDY: Fix incorrect copyright date.

--- src/include/a3/platform/features.h
+++ src/include/a3/platform/features.h
@@ -1,7 +1,7 @@
 /*
  * FEATURES -- Compile-time feature detection.
  *
- * Copyright (c) 2020-2021, Alex O'Brien <3541ax@gmail.com>
+ * Copyright (c) 2021, Alex O'Brien <3541ax@gmail.com>
  *
  * This file is licensed under the BSD 3-clause license. See the LICENSE file in
  * the project root for details.
