770920caf7dd379d5a23f8c3ab37876333a04922
Alex O'Brien <3541ax@gmail.com>
2021-11-01 13:47:59 +1100

    BUILD: nix: Add envrc, fix clangd.

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  packages = with pkgs; [ gcc clang meson gtest pkgconf ninja ];
+  # NOTE: The placement of clang-tools /before/ llvmPackages.clang is critical. Otherwise, the
+  # latter package will provide an un-wrapped `clangd`, which is unable to find system headers.
+  packages = with pkgs; [ gcc clang-tools llvmPackages_latest.clang meson gtest pkg-config ninja ];
 }
