2424e1db86bb2a2f3bfa22bb8a38805bad57e8b3
Alex O'Brien <3541ax@gmail.com>
2021-11-01 13:49:00 +1100

    TIDY: Formatting in shell.nix.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,15 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  # NOTE: The placement of clang-tools /before/ llvmPackages.clang is critical. Otherwise, the
-  # latter package will provide an un-wrapped `clangd`, which is unable to find system headers.
-  packages = with pkgs; [ gcc clang-tools llvmPackages_latest.clang meson gtest pkg-config ninja ];
+  packages = with pkgs; [
+    gcc
+    # NOTE: The placement of clang-tools /before/ llvmPackages.clang is critical. Otherwise, the
+    # latter package will provide an un-wrapped `clangd`, which is unable to find system headers.
+    clang-tools
+    llvmPackages_latest.clang
+    meson
+    gtest
+    pkg-config
+    ninja
+  ];
 }
