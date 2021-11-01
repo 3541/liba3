ba73c871224e9a1e2a0b9b7f153e59da4984c904
Alex O'Brien <3541ax@gmail.com>
2021-11-01 21:48:46 +1100

    BUILD: nix: Add gdb and rr.

--- shell.nix
+++ shell.nix
@@ -3,6 +3,8 @@
 pkgs.mkShell {
   packages = with pkgs; [
     gcc
+    gdb
+    rr
     # NOTE: The placement of clang-tools /before/ llvmPackages.clang is critical. Otherwise, the
     # latter package will provide an un-wrapped `clangd`, which is unable to find system headers.
     clang-tools
