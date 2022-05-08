95cceea5d731a3faad912222d506e4ba24f91500
Alex O'Brien <3541ax@gmail.com>
2022-05-08 11:27:03 +1000

    SLL: Style and assert cleanup.

--- src/include/a3/sll.h
+++ src/include/a3/sll.h
@@ -18,6 +18,9 @@
 
 #include <assert.h>
 #include <stdbool.h>
+#include <stddef.h>
+
+#include <a3/util.h>
 
 /// A list. Keeps track of both the head and tail in order to enable the list to be used as a queue.
 #define A3_SLL(NAME, TY)                                                                           \
@@ -27,91 +30,113 @@
     }
 
 /// A list link. To make a type usable in a list, simply add a member of this type.
-#define A3_S_LINK(TY)                                                                              \
+#define A3_SLL_LINK(TY)                                                                            \
     struct {                                                                                       \
         TY* next;                                                                                  \
     }
 
 /// Initialize a list.
-#define A3_SLL_INIT(L)                                                                             \
+#define A3_SLL_INIT(LIST)                                                                          \
     do {                                                                                           \
-        assert(L);                                                                                 \
-        (L)->head = NULL;                                                                          \
-        (L)->end  = &(L)->head;                                                                    \
+        assert(LIST);                                                                              \
+                                                                                                   \
+        (LIST)->head = NULL;                                                                       \
+        (LIST)->end  = &(LIST)->head;                                                              \
     } while (false)
 
 /// Destroy a list. Does nothing to the contents.
 #define A3_SLL_DESTROY A3_SLL_INIT
 
 /// Get the first element of the list. Otherwise, returns `NULL`.
-#define A3_SLL_HEAD(L) ((L)->head)
-
-/// Get the following element.
-#define A3_SLL_NEXT(E, F) ((E)->F.next)
+#define A3_SLL_HEAD(LIST) ((LIST)->head)
 
 /// Check whether the list is empty.
-#define A3_SLL_IS_EMPTY(L) (!A3_SLL_HEAD(L))
+#define A3_SLL_IS_EMPTY(LIST) (!A3_SLL_HEAD(LIST))
+
+/// Get the last element of the list. Otherwise, returns `NULL`. THIS MAY BE OUT OF DATE DUE TO
+/// CALLS TO ::A3_SLL_INSERT_AFTER.
+#define A3_SLL_END(LIST, TY, FIELD)                                                                \
+    (!A3_SLL_IS_EMPTY(LIST) ? A3_CONTAINER_OF((LIST)->end, TY, FIELD) : NULL)
+
+/// Get the following element.
+#define A3_SLL_NEXT(ELEM, FIELD) ((ELEM)->FIELD.next)
 
 /// Insert the given element after the link.
-#define A3_SLL_INSERT_AFTER(A, B, F)                                                               \
+#define A3_SLL_INSERT_AFTER(ELEM, NEXT_ELEM, FIELD)                                                \
     do {                                                                                           \
-        assert(A);                                                                                 \
-        assert(B);                                                                                 \
-        A3_SLL_NEXT(B, F) = A3_SLL_NEXT(A, F);                                                     \
-        A3_SLL_NEXT(A, F) = (B);                                                                   \
+        assert(ELEM);                                                                              \
+        assert(NEXT_ELEM);                                                                         \
+                                                                                                   \
+        A3_SLL_NEXT(NEXT_ELEM, FIELD) = A3_SLL_NEXT(ELEM, FIELD);                                  \
+        A3_SLL_NEXT(ELEM, FIELD)      = (NEXT_ELEM);                                               \
     } while (false)
 
 /// Remove an item from the head of the list. See also ::A3_SLL_PUSH.
-#define A3_SLL_POP(L, F)                                                                           \
+#define A3_SLL_POP(LIST, FIELD)                                                                    \
     do {                                                                                           \
-        assert(L);                                                                                 \
-        A3_SLL_HEAD(L) = A3_SLL_NEXT(A3_SLL_HEAD(L), F);                                           \
-        if (!A3_SLL_HEAD(L))                                                                       \
-            (L)->end = &A3_SLL_HEAD(L);                                                            \
+        assert(LIST);                                                                              \
+        assert(A3_SLL_HEAD(LIST));                                                                 \
+                                                                                                   \
+        A3_SLL_HEAD(LIST) = A3_SLL_NEXT(A3_SLL_HEAD(LIST), FIELD);                                 \
+        if (!A3_SLL_HEAD(LIST))                                                                    \
+            (LIST)->end = &A3_SLL_HEAD(LIST);                                                      \
     } while (false)
 
 /// Remove the given element from the list. Linear time with respect to the length of the list.
-#define A3_SLL_REMOVE(L, T, E, F)                                                                  \
+#define A3_SLL_REMOVE(LIST, ELEM, TY, FIELD)                                                       \
     do {                                                                                           \
-        assert(L);                                                                                 \
-        assert(E);                                                                                 \
-        if ((E) == A3_SLL_HEAD(L)) {                                                               \
-            A3_SLL_POP(L, F);                                                                      \
+        assert(LIST);                                                                              \
+        assert(ELEM);                                                                              \
+                                                                                                   \
+        if ((ELEM) == A3_SLL_HEAD(LIST)) {                                                         \
+            A3_SLL_POP(LIST, FIELD);                                                               \
             break;                                                                                 \
         }                                                                                          \
                                                                                                    \
-        T* _it = A3_SLL_HEAD(L);                                                                   \
-        while (A3_SLL_NEXT(_it, F) != (E))                                                         \
-            _it = A3_SLL_NEXT(_it, F);                                                             \
-        A3_SLL_NEXT(_it, F) = A3_SLL_NEXT(A3_SLL_NEXT(_it, F), F);                                 \
-        if (!A3_SLL_NEXT(_it, F))                                                                  \
-            (L)->end = &A3_SLL_NEXT(_it, F);                                                       \
+        TY* _it = A3_SLL_HEAD(LIST);                                                               \
+        while (_it && A3_SLL_NEXT(_it, FIELD) != (ELEM))                                           \
+            _it = A3_SLL_NEXT(_it, FIELD);                                                         \
+        assert(_it);                                                                               \
+                                                                                                   \
+        A3_SLL_NEXT(_it, FIELD) = A3_SLL_NEXT(A3_SLL_NEXT(_it, FIELD), FIELD);                     \
+        if (!A3_SLL_NEXT(_it, FIELD))                                                              \
+            (LIST)->end = &A3_SLL_NEXT(_it, FIELD);                                                \
     } while (false)
 
 /// Add an item to the head of the list. See also ::A3_SLL_POP.
-#define A3_SLL_PUSH(L, E, F)                                                                       \
+#define A3_SLL_PUSH(LIST, ELEM, FIELD)                                                             \
     do {                                                                                           \
-        assert(L);                                                                                 \
-        assert(E);                                                                                 \
-        A3_SLL_NEXT(E, F) = A3_SLL_HEAD(L);                                                        \
-        if (!A3_SLL_NEXT(E, F))                                                                    \
-            (L)->end = &A3_SLL_NEXT(E, F);                                                         \
-        (L)->head = (E);                                                                           \
+        assert(LIST);                                                                              \
+        assert(ELEM);                                                                              \
+                                                                                                   \
+        A3_SLL_NEXT(ELEM, FIELD) = A3_SLL_HEAD(LIST);                                              \
+        if (!A3_SLL_NEXT(ELEM, FIELD))                                                             \
+            (LIST)->end = &A3_SLL_NEXT(ELEM, FIELD);                                               \
+        (LIST)->head = (ELEM);                                                                     \
+    } while (false)
+
+/// Fix up the list's end pointer.
+#define A3_SLL_FIXUP(LIST, FIELD)                                                                  \
+    do {                                                                                           \
+        assert(LIST);                                                                              \
+                                                                                                   \
+        while (*(LIST)->end)                                                                       \
+            (LIST)->end = &A3_SLL_NEXT(*(LIST)->end, FIELD);                                       \
     } while (false)
 
 /// \brief Add an item to the end of the list. See also ::A3_SLL_DEQUEUE.
 ///
 /// This fixes up the list's end pointer, if necessary, and so may have worst-case linear time. This
 /// is, however, unlikely.
-#define A3_SLL_ENQUEUE(L, E, F)                                                                    \
+#define A3_SLL_ENQUEUE(LIST, ELEM, FIELD)                                                          \
     do {                                                                                           \
-        assert(L);                                                                                 \
-        assert(E);                                                                                 \
-        while (*(L)->end)                                                                          \
-            (L)->end = &A3_SLL_NEXT(*(L)->end, F);                                                 \
-        A3_SLL_NEXT(E, F) = NULL;                                                                  \
-        *(L)->end         = (E);                                                                   \
-        (L)->end          = &A3_SLL_NEXT(E, F);                                                    \
+        assert(LIST);                                                                              \
+        assert(ELEM);                                                                              \
+                                                                                                   \
+        A3_SLL_FIXUP(LIST, FIELD);                                                                 \
+        A3_SLL_NEXT(ELEM, FIELD) = NULL;                                                           \
+        *(LIST)->end             = (ELEM);                                                         \
+        (LIST)->end              = &A3_SLL_NEXT(ELEM, FIELD);                                      \
     } while (false)
 
 /// Remove an item from the head of the list.
--- test/sll.cc
+++ test/sll.cc
@@ -10,10 +10,10 @@ namespace sll {
 using namespace testing;
 
 struct SLLNode {
-    size_t data;                // NOLINT(misc-non-private-member-variables-in-classes)
-    A3_S_LINK(SLLNode) link {}; // NOLINT(misc-non-private-member-variables-in-classes)
+    unsigned int data;            // NOLINT(misc-non-private-member-variables-in-classes)
+    A3_SLL_LINK(SLLNode) link {}; // NOLINT(misc-non-private-member-variables-in-classes)
 
-    explicit SLLNode(size_t d) : data { d } {}
+    explicit SLLNode(unsigned int d) : data { d } {}
 };
 
 class SLLTest : public Test {
@@ -30,8 +30,8 @@ TEST_F(SLLTest, push_pop) {
     A3_SLL_PUSH(&list, t, link);
 
     auto* p = A3_SLL_HEAD(&list);
-    EXPECT_EQ(p->data, 1234ULL);
-    EXPECT_FALSE(p->link.next);
+    EXPECT_EQ(p->data, 1234U);
+    EXPECT_THAT(A3_SLL_NEXT(p, link), IsNull());
     EXPECT_EQ(p, t);
 
     A3_SLL_POP(&list, link);
@@ -41,15 +41,15 @@ TEST_F(SLLTest, push_pop) {
 }
 
 TEST_F(SLLTest, many_insertions) {
-    for (size_t i = 1; i <= 128; i++) {
+    for (unsigned int i = 1; i <= 128; i++) {
         auto* n = new SLLNode { i };
         A3_SLL_PUSH(&list, n, link);
     }
 
-    size_t i = 128;
+    unsigned int i = 128;
     A3_SLL_FOR_EACH(SLLNode, node, &list, link) { EXPECT_EQ(node->data, i--); }
 
-    EXPECT_EQ(i, 0ULL);
+    EXPECT_EQ(i, 0U);
 
     while (auto* p = A3_SLL_HEAD(&list)) {
         A3_SLL_POP(&list, link);
@@ -64,7 +64,8 @@ TEST_F(SLLTest, many_insertions) {
     i = 1;
     A3_SLL_FOR_EACH(SLLNode, node, &list, link) { EXPECT_EQ(node->data, i++); }
 
-    EXPECT_EQ(i, 129ULL);
+    EXPECT_EQ(i, 129U);
+    EXPECT_THAT(A3_SLL_NEXT(A3_SLL_HEAD(&list), link)->data, Eq(2U));
 
     while (auto* p = A3_SLL_HEAD(&list)) {
         A3_SLL_POP(&list, link);
@@ -75,6 +76,130 @@ TEST_F(SLLTest, many_insertions) {
 TEST_F(SLLTest, for_each_empty) {
     A3_SLL_FOR_EACH(SLLNode, node, &list, link) {
         EXPECT_THAT(false, IsTrue()) << "foreach should not loop when list is empty.";
+    }
+}
+
+TEST_F(SLLTest, insert_after) {
+    for (unsigned int i = 1; i <= 128; i++) {
+        auto* n = new SLLNode { i };
+        A3_SLL_ENQUEUE(&list, n, link);
+    }
+
+    auto* end = A3_SLL_END(&list, SLLNode, link);
+    EXPECT_THAT(end, NotNull());
+    EXPECT_THAT(end->data, Eq(128U));
+
+    for (unsigned int i = 130; i <= 256; i++) {
+        auto* n = new SLLNode { i };
+        A3_SLL_ENQUEUE(&list, n, link);
+    }
+
+    auto* next = new SLLNode { 129 };
+    A3_SLL_INSERT_AFTER(end, next, link);
+
+    unsigned int i = 1;
+    A3_SLL_FOR_EACH(SLLNode, node, &list, link) { EXPECT_EQ(node->data, i++); }
+    EXPECT_THAT(i, Eq(257U));
+
+    while (auto* p = A3_SLL_HEAD(&list)) {
+        A3_SLL_POP(&list, link);
+        delete p;
+    }
+}
+
+TEST_F(SLLTest, remove_first) {
+    for (unsigned int i = 1; i <= 128; i++) {
+        auto* n = new SLLNode { i };
+        A3_SLL_ENQUEUE(&list, n, link);
+    }
+
+    auto* first = A3_SLL_HEAD(&list);
+    ASSERT_THAT(first, NotNull());
+    EXPECT_THAT(first->data, Eq(1U));
+
+    A3_SLL_REMOVE(&list, first, SLLNode, link);
+    delete first;
+
+    first = A3_SLL_HEAD(&list);
+    ASSERT_THAT(first, NotNull());
+    EXPECT_THAT(first->data, Eq(2U));
+
+    unsigned int i = 2;
+    while (auto* p = A3_SLL_HEAD(&list)) {
+        A3_SLL_POP(&list, link);
+        EXPECT_EQ(p->data, i++);
+        delete p;
+    }
+}
+
+TEST_F(SLLTest, remove_last) {
+    for (unsigned int i = 1; i <= 128; i++) {
+        auto* n = new SLLNode { i };
+        A3_SLL_ENQUEUE(&list, n, link);
+    }
+
+    auto* last = A3_SLL_END(&list, SLLNode, link);
+    ASSERT_THAT(last, NotNull());
+    EXPECT_THAT(last->data, Eq(128U));
+
+    A3_SLL_REMOVE(&list, last, SLLNode, link);
+    delete last;
+
+    last = A3_SLL_END(&list, SLLNode, link);
+    ASSERT_THAT(last, NotNull());
+    EXPECT_THAT(last->data, Eq(127U));
+
+    unsigned int i = 1;
+    while (auto* p = A3_SLL_HEAD(&list)) {
+        A3_SLL_POP(&list, link);
+        EXPECT_THAT(p->data, Eq(i++));
+        EXPECT_THAT(p->data, Lt(128U));
+        delete p;
+    }
+}
+
+TEST_F(SLLTest, remove_mid) {
+    for (unsigned int i = 1; i <= 128; i++) {
+        auto* n = new SLLNode { i };
+        A3_SLL_ENQUEUE(&list, n, link);
+    }
+
+    SLLNode* mid = nullptr;
+    A3_SLL_FOR_EACH(SLLNode, n, &list, link) {
+        if (n->data == 64U) {
+            mid = n;
+            break;
+        }
+    }
+    ASSERT_THAT(mid, NotNull());
+    EXPECT_THAT(mid->data, Eq(64U));
+
+    A3_SLL_REMOVE(&list, mid, SLLNode, link);
+    delete mid;
+
+    SLLNode* before = nullptr;
+    SLLNode* after  = nullptr;
+    A3_SLL_FOR_EACH(SLLNode, n, &list, link) {
+        if (n->data == 63U) {
+            before = n;
+            after  = A3_SLL_NEXT(before, link);
+            break;
+        }
+    }
+    ASSERT_THAT(before, NotNull());
+    ASSERT_THAT(after, NotNull());
+    EXPECT_THAT(before->data, Eq(63U));
+    EXPECT_THAT(after->data, Eq(65U));
+
+    unsigned int i = 1;
+    while (auto* p = A3_SLL_HEAD(&list)) {
+        A3_SLL_POP(&list, link);
+        EXPECT_THAT(p->data, Eq(i++));
+        EXPECT_THAT(p->data, Ne(64U));
+
+        if (i == 64)
+            i++;
+        delete p;
     }
 }
 
