370e533fb5a2d855a1a00c00e9c007fbcdefc8a4
Alex O'Brien <3541@3541.website>
2022-05-24 22:18:18 +1000

    SPMC: Add prototype SPMC queue.

---
+++ src/include/a3/spmc.h
@@ -0,0 +1,58 @@
+/*
+ * SPMC -- A finite-size thread-safe single-producer multiple-consumer queue.
+ *
+ * Copyright (c) 2022, Alex O'Brien <3541@3541.website>
+ *
+ * This file is licensed under the BSD 3-clause license. See the LICENSE file in
+ * the project root for details.
+ */
+
+#pragma once
+
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <threads.h>
+
+#define A3_ATOMIC_PTR(TY) TY* _Atomic
+
+/// An SPMC queue. See ::a3_spmc_init.
+typedef struct A3Spmc {
+    A3_ATOMIC_PTR(void) * data; //< The actual backing array.
+    size_t cap;                 //< The queue's capacity.
+    size_t mask;
+
+    atomic_size_t head; //< The index of the oldest element in the queue.
+    atomic_size_t end;  //< The index of the next free slot. If head == end, queue is empty.
+
+#ifndef NDEBUG
+    thrd_t producer; //< The producer thread. Used only for debug assertions.
+#endif
+} A3Spmc;
+
+/// Initialize a queue. Capacity will be aligned up to the nearest power of two. When debug
+/// assertions are enabled, it is assumed that this is called by the producer. If not, manually
+/// override the producer field.
+void a3_spmc_init(A3Spmc*, size_t cap);
+
+/// Allocate and initialize a new queue.
+A3Spmc* a3_spmc_new(size_t cap);
+
+/// Destroy a queue. Frees the backing storage, but does nothing to the contents. NOT THREAD SAFE.
+/// Ensure all consumers are gone before calling.
+void a3_spmc_destroy(A3Spmc*);
+
+/// Free and destroy an allocated queue.
+void a3_spmc_free(A3Spmc*);
+
+/// Try to remove the first element from the queue, if any. May fail spuriously.
+void* a3_spmc_try_dequeue(A3Spmc*);
+
+/// Block until an element is available, then return it.
+void* a3_spmc_dequeue(A3Spmc*);
+
+/// Enqueue an element. Returns `true` if successful, `false` if not. NOT THREAD SAFE. Call only
+/// from the consumer thread.
+bool a3_spmc_try_enqueue(A3Spmc*, void* elem);
+
+/// Enqueue an element. Blocks until complete. NOT THREAD SAFE. Call only from the consumer thread.
+void a3_spmc_enqueue(A3Spmc*, void* elem);
---
+++ src/spmc.c
@@ -0,0 +1,127 @@
+/*
+ * SPMC -- A finite-size thread-safe single-producer multiple-consumer queue.
+ *
+ * Copyright (c) 2022, Alex O'Brien <3541@3541.website>
+ *
+ * This file is licensed under the BSD 3-clause license. See the LICENSE file in
+ * the project root for details.
+ */
+
+#include <assert.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <threads.h>
+
+#include <a3/spmc.h>
+#include <a3/util.h>
+
+void a3_spmc_init(A3Spmc* q, size_t cap) {
+    assert(q);
+
+    q->cap = 1;
+    while (q->cap < cap)
+        q->cap <<= 1;
+    q->mask = q->cap - 1;
+
+    A3_UNWRAPN(q->data, calloc(q->cap, sizeof(*q->data)));
+    atomic_init(&q->head, 0);
+    atomic_init(&q->end, 0);
+
+#ifndef NDEBUG
+    q->producer = thrd_current();
+#endif
+}
+
+A3Spmc* a3_spmc_new(size_t cap) {
+    A3_UNWRAPNI(A3Spmc*, ret, calloc(1, sizeof(*ret)));
+    a3_spmc_init(ret, cap);
+
+    return ret;
+}
+
+void a3_spmc_destroy(A3Spmc* q) {
+    assert(q);
+    assert(thrd_equal(q->producer, thrd_current()));
+    assert(q->head == q->end);
+
+    free(q->data);
+}
+
+void a3_spmc_free(A3Spmc* q) {
+    a3_spmc_destroy(q);
+    free(q);
+}
+
+static size_t a3_spmc_index(A3Spmc* q, size_t ticket) {
+    assert(q);
+
+    return ticket & q->mask;
+}
+
+void* a3_spmc_try_dequeue(A3Spmc* q) {
+    assert(q);
+
+    size_t end  = atomic_load_explicit(&q->end, memory_order_acquire);
+    size_t head = atomic_load_explicit(&q->head, memory_order_acquire);
+
+    for (size_t i = 0; i < 5; i++) {
+        if (head >= end)
+            return NULL;
+
+        if (atomic_compare_exchange_strong_explicit(&q->head, &head, head + 1, memory_order_acq_rel,
+                                                    memory_order_acquire))
+            goto found;
+    }
+    return NULL;
+
+found:
+    // TODO: seq_cst?
+    return atomic_exchange_explicit(&q->data[a3_spmc_index(q, head)], NULL, memory_order_acq_rel);
+}
+
+void* a3_spmc_dequeue(A3Spmc* q) {
+    assert(q);
+
+    size_t head = atomic_fetch_add_explicit(&q->head, 1, memory_order_acq_rel);
+
+    // TODO: Smarter ways to block.
+    while (head >= atomic_load_explicit(&q->end, memory_order_acquire))
+        thrd_yield();
+
+    return atomic_exchange_explicit(&q->data[a3_spmc_index(q, head)], NULL, memory_order_acq_rel);
+}
+
+bool a3_spmc_try_enqueue(A3Spmc* q, void* elem) {
+    assert(q);
+    assert(thrd_equal(q->producer, thrd_current()));
+
+    size_t end  = atomic_load_explicit(&q->end, memory_order_relaxed);
+    size_t head = atomic_load_explicit(&q->head, memory_order_acquire);
+    if (end >= head + q->cap)
+        return false;
+
+    size_t end_index = a3_spmc_index(q, end);
+    if (atomic_load_explicit(&q->data[end_index], memory_order_acquire))
+        return false;
+
+    atomic_store_explicit(&q->data[end_index], elem, memory_order_release);
+    atomic_fetch_add_explicit(&q->end, 1, memory_order_release);
+
+    return true;
+}
+
+void a3_spmc_enqueue(A3Spmc* q, void* elem) {
+    assert(q);
+    assert(thrd_equal(q->producer, thrd_current()));
+
+    size_t end = atomic_load_explicit(&q->end, memory_order_relaxed);
+    while (end >= atomic_load_explicit(&q->head, memory_order_acquire) + q->cap)
+        thrd_yield();
+
+    size_t end_index = a3_spmc_index(q, end);
+    while (atomic_load_explicit(&q->data[end_index], memory_order_acquire))
+        thrd_yield();
+
+    atomic_store_explicit(&q->data[end_index], elem, memory_order_release);
+    atomic_fetch_add_explicit(&q->end, 1, memory_order_release);
+}
