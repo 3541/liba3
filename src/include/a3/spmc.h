6675392040b11a64d96b2142912aaeeac7601738
Alex O'Brien <3541@3541.website>
2022-05-24 23:40:01 +1000

    SPMC: Missing A3_EXPORT.

--- src/include/a3/spmc.h
+++ src/include/a3/spmc.h
@@ -12,6 +12,8 @@
 #include <a3/shim/atomic.h>
 #include <stdbool.h>
 
+#include <a3/types.h>
+
 #define A3_ATOMIC_PTR(TY) TY* _Atomic
 
 /// An SPMC queue. See ::a3_spmc_init.
@@ -29,27 +31,27 @@ typedef struct A3Spmc {
 /// Initialize a queue. Capacity will be aligned up to the nearest power of two. When debug
 /// assertions are enabled, it is assumed that this is called by the producer. If not, manually
 /// override the producer field.
-void a3_spmc_init(A3Spmc*, size_t cap);
+A3_EXPORT void a3_spmc_init(A3Spmc*, size_t cap);
 
 /// Allocate and initialize a new queue.
-A3Spmc* a3_spmc_new(size_t cap);
+A3_EXPORT A3Spmc* a3_spmc_new(size_t cap);
 
 /// Destroy a queue. Frees the backing storage, but does nothing to the contents. NOT THREAD SAFE.
 /// Ensure all consumers are gone before calling.
-void a3_spmc_destroy(A3Spmc*);
+A3_EXPORT void a3_spmc_destroy(A3Spmc*);
 
 /// Free and destroy an allocated queue.
-void a3_spmc_free(A3Spmc*);
+A3_EXPORT void a3_spmc_free(A3Spmc*);
 
 /// Try to remove the first element from the queue, if any. May fail spuriously.
-void* a3_spmc_try_dequeue(A3Spmc*);
+A3_EXPORT void* a3_spmc_try_dequeue(A3Spmc*);
 
 /// Block until an element is available, then return it.
-void* a3_spmc_dequeue(A3Spmc*);
+A3_EXPORT void* a3_spmc_dequeue(A3Spmc*);
 
 /// Enqueue an element. Returns `true` if successful, `false` if not. NOT THREAD SAFE. Call only
 /// from the consumer thread.
-bool a3_spmc_try_enqueue(A3Spmc*, void* elem);
+A3_EXPORT bool a3_spmc_try_enqueue(A3Spmc*, void* elem);
 
 /// Enqueue an element. Blocks until complete. NOT THREAD SAFE. Call only from the consumer thread.
-void a3_spmc_enqueue(A3Spmc*, void* elem);
+A3_EXPORT void a3_spmc_enqueue(A3Spmc*, void* elem);
