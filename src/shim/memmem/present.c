2edc3f33fb8a8f197f727ca7be673b41cff864b5
Alex O'Brien <3541@3541.website>
2024-04-03 11:17:15 +1100

    TEST: Add test for memmem shim.

--- src/shim/memmem/present.c
+++ src/shim/memmem/present.c
@@ -5,15 +5,14 @@
  *
  * This file is licensed under the BSD 3-clause license. See the LICENSE file in the project root
  * for details.
- *
- * Windows does not provide memmem.
  */
 
 #define _GNU_SOURCE
 
-#include <a3/shim/memmem.h>
 #include <stddef.h>
 #include <string.h>
+
+#include <a3/shim/memmem.h>
 
 void* a3_shim_memmem(void const* haystack, size_t haystack_len, void const* needle,
                      size_t needle_len) {
---
+++ test/shim/memmem.cc
@@ -0,0 +1,43 @@
+#include <gmock/gmock.h>
+
+#include <a3/shim/memmem.h>
+
+namespace a3::test {
+
+using namespace testing;
+
+struct MemmemTest : public Test {
+protected:
+    std::vector<std::byte> m_buf;
+
+    MemmemTest();
+};
+
+MemmemTest::MemmemTest() {
+    m_buf.reserve('z' - 'a' + 1);
+
+    for (std::underlying_type_t<std::byte> c = 'a'; c <= 'z'; ++c)
+        m_buf.push_back(std::byte{c});
+}
+
+TEST_F(MemmemTest, missing) {
+    EXPECT_THAT(::a3_shim_memmem(m_buf.data(), m_buf.size(), "asdf", 4), IsNull());
+}
+
+TEST_F(MemmemTest, present) {
+    auto const* res = ::a3_shim_memmem(m_buf.data(), m_buf.size(), "xyz", 3);
+    EXPECT_THAT(res, Eq(&m_buf[m_buf.size() - 3]));
+    ASSERT_THAT(res, NotNull());
+    EXPECT_THAT(*static_cast<char const*>(res), Eq('x'));
+}
+
+TEST_F(MemmemTest, multiple) {
+    m_buf.push_back(std::byte{'a'});
+    m_buf.push_back(std::byte{'b'});
+
+    auto const* res = ::a3_shim_memmem(m_buf.data(), m_buf.size(), "ab", 2);
+    EXPECT_THAT(res, Eq(m_buf.data()));
+    EXPECT_THAT(*static_cast<char const*>(res), Eq('a'));
+}
+
+} // namespace a3::test
