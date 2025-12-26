b23e2044dd73cf9cddf074fcc4e87035959b2792
Alex O'Brien <3541@3541.website>
2025-12-26 11:17:42 +1100

    BUILD: Add missing flags for Bazel.

--- a3/shim/attributes.h
+++ a3/shim/attributes.h
@@ -7,14 +7,14 @@
  * for details.
  */
 
- #pragma once
+#pragma once
 
- #include "a3/shim/attribute.h"
+#include "a3/shim/attribute.h"
 
- #if defined(__cplusplus) && A3_HAS_CPP_ATTR(gnu::always_inline)
- #define A3_ALWAYS_INLINE [[gnu::always_inline]] inline
- #elif A3_HAS_ATTR(__always_inline__) || defined(__GNUC__)
- #define A3_ALWAYS_INLINE static inline __attribute__((__always_inline__))
- #else
- #define A3_ALWAYS_INLINE static inline
- #endif
+#if defined(__cplusplus) && A3_HAS_CPP_ATTR(gnu::always_inline)
+#define A3_ALWAYS_INLINE [[gnu::always_inline]] inline
+#elif A3_HAS_ATTR(__always_inline__) || defined(__GNUC__)
+#define A3_ALWAYS_INLINE __attribute__((__always_inline__)) static inline
+#else
+#define A3_ALWAYS_INLINE static inline
+#endif
--- a3/util.h
+++ a3/util.h
@@ -17,7 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <a3/log_internal.h>
+#include <a3/detail/log.h>
 #include <a3/macro.h>
 #include <a3/shim/cpp.h>
 #include <a3/shim/likely.h>
