9cafa98b5224bd3051c958e0113561467d747b2a
Alex O'Brien <3541@3541.website>
2025-12-26 12:07:29 +1100

    BUILD: Port sll.

--- test/sll.cc
+++ test/sll.cc
@@ -1,11 +1,9 @@
+#include "a3/sll.h"
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <a3/sll.h>
-
-namespace a3 {
-namespace test {
-namespace sll {
+namespace a3::test::sll {
 
 using namespace testing;
 
@@ -13,7 +11,7 @@ struct SLLNode {
     unsigned int data;            // NOLINT(misc-non-private-member-variables-in-classes)
     A3_SLL_LINK(SLLNode) link {}; // NOLINT(misc-non-private-member-variables-in-classes)
 
-    explicit SLLNode(unsigned int d) : data { d } {}
+    explicit SLLNode(unsigned int d) : data{d} {}
 };
 
 class SLLTest : public Test {
@@ -26,7 +24,7 @@ protected:
 TEST_F(SLLTest, init) { EXPECT_TRUE(A3_SLL_IS_EMPTY(&list)); }
 
 TEST_F(SLLTest, push_pop) {
-    auto* t = new SLLNode { 1234 };
+    auto* t = new SLLNode{1234};
     A3_SLL_PUSH(&list, t, link);
 
     auto* p = A3_SLL_HEAD(&list);
@@ -42,12 +40,14 @@ TEST_F(SLLTest, push_pop) {
 
 TEST_F(SLLTest, many_insertions) {
     for (unsigned int i = 1; i <= 128; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_PUSH(&list, n, link);
     }
 
     unsigned int i = 128;
-    A3_SLL_FOR_EACH (SLLNode, node, &list, link) { EXPECT_EQ(node->data, i--); }
+    A3_SLL_FOR_EACH (SLLNode, node, &list, link) {
+        EXPECT_EQ(node->data, i--);
+    }
 
     EXPECT_EQ(i, 0U);
 
@@ -57,12 +57,14 @@ TEST_F(SLLTest, many_insertions) {
     }
 
     for (i = 1; i <= 128; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_ENQUEUE(&list, n, link);
     }
 
     i = 1;
-    A3_SLL_FOR_EACH (SLLNode, node, &list, link) { EXPECT_EQ(node->data, i++); }
+    A3_SLL_FOR_EACH (SLLNode, node, &list, link) {
+        EXPECT_EQ(node->data, i++);
+    }
 
     EXPECT_EQ(i, 129U);
     EXPECT_THAT(A3_SLL_NEXT(A3_SLL_HEAD(&list), link)->data, Eq(2U));
@@ -81,7 +83,7 @@ TEST_F(SLLTest, for_each_empty) {
 
 TEST_F(SLLTest, insert_after) {
     for (unsigned int i = 1; i <= 128; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_ENQUEUE(&list, n, link);
     }
 
@@ -90,15 +92,17 @@ TEST_F(SLLTest, insert_after) {
     EXPECT_THAT(end->data, Eq(128U));
 
     for (unsigned int i = 130; i <= 256; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_ENQUEUE(&list, n, link);
     }
 
-    auto* next = new SLLNode { 129 };
+    auto* next = new SLLNode{129};
     A3_SLL_INSERT_AFTER(end, next, link);
 
     unsigned int i = 1;
-    A3_SLL_FOR_EACH (SLLNode, node, &list, link) { EXPECT_EQ(node->data, i++); }
+    A3_SLL_FOR_EACH (SLLNode, node, &list, link) {
+        EXPECT_EQ(node->data, i++);
+    }
     EXPECT_THAT(i, Eq(257U));
 
     while (auto* p = A3_SLL_HEAD(&list)) {
@@ -109,7 +113,7 @@ TEST_F(SLLTest, insert_after) {
 
 TEST_F(SLLTest, remove_first) {
     for (unsigned int i = 1; i <= 128; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_ENQUEUE(&list, n, link);
     }
 
@@ -134,7 +138,7 @@ TEST_F(SLLTest, remove_first) {
 
 TEST_F(SLLTest, remove_last) {
     for (unsigned int i = 1; i <= 128; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_ENQUEUE(&list, n, link);
     }
 
@@ -160,7 +164,7 @@ TEST_F(SLLTest, remove_last) {
 
 TEST_F(SLLTest, remove_mid) {
     for (unsigned int i = 1; i <= 128; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_ENQUEUE(&list, n, link);
     }
 
@@ -205,7 +209,7 @@ TEST_F(SLLTest, remove_mid) {
 
 TEST_F(SLLTest, nested_for_each) {
     for (unsigned i = 1; i <= 128; i++) {
-        auto* n = new SLLNode { i };
+        auto* n = new SLLNode{i};
         A3_SLL_ENQUEUE(&list, n, link);
     }
 
@@ -222,6 +226,4 @@ TEST_F(SLLTest, nested_for_each) {
         delete node;
 }
 
-} // namespace sll
-} // namespace test
-} // namespace a3
+} // namespace a3::test::sll
