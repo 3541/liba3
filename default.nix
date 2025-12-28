270d49c70ebaf88262f9276183f49b6cb30aa96e
Alex O'Brien <3541@3541.website>
2025-12-28 12:28:03 +1100

    BUILD: Switch to Nix-provided Bazel.

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/7a0f7bfacda008fcd0ae46ef106d7c7dfe53596e";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import ./nixpkgs.nix {};
 in
 
 pkgs.mkShellNoCC {
   nativeBuildInputs = with pkgs; [
-    bazelisk
+    bazel_8
     buildifier
     meson
     ninja
