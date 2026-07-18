c81a6ff9b6396f62a87d293cd423eba522bd42e9
Alex O'Brien <3541@3541.website>
2026-07-18 11:37:45 +0800

    BUILD: Bump Bazel and update FreeBSD CI.

--- ci/tools/freebsd_bazel.sh
+++ ci/tools/freebsd_bazel.sh
@@ -1,26 +1,23 @@
-# Build Bazel 8 for FreeBSD.
+# Build Bazel 9 for FreeBSD.
 #
-# Copyright (c) 2025, Alex O'Brien <3541@3541.website>
+# Copyright (c) 2025, 2026, Alex O'Brien <3541@3541.website>
 #
 # This file is licensed under the BSD 3-clause license. See the LICENSE file in
 # the project root for details.
 #
-# Workaround until the port is added: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=287546
+# Workaround until binary packages are published.
 
 #!/usr/bin/env sh
 
 set -e
 
-if [ ! -d /usr/ports/devel/bazel8 ]; then
-    patch_file="$(mktemp)"
-    curl https://bz-attachments.freebsd.org/attachment.cgi?id=265221 > "$patch_file"
-
-    cd /usr/ports
-    git apply "$patch_file"
-    rm "$patch_file"
+if ! -f /etc/make.conf; then
+    touch /etc/make.conf
 fi
 
-echo 'USE_PACKAGE_DEPENDS="YES"' >> /etc/make.conf
+if grep -qv USE_PACKAGE_DEPENDS /etc/make.conf; then
+    echo 'USE_PACKAGE_DEPENDS="YES"' >> /etc/make.conf
+fi
 
-cd /usr/ports/devel/bazel8
+cd /usr/ports/devel/bazel9
 make install -DBATCH
