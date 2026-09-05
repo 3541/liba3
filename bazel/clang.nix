383a2c6493ecbab140f20abd7ae71c9a2a05c84c
Alex O'Brien <3541@3541.website>
2026-09-05 18:46:45 +0800

    BUILD: Bump Clang to 22.

--- bazel/clang.nix
+++ bazel/clang.nix
@@ -3,7 +3,7 @@ let
 in
 import ./cc.nix rec {
   inherit pkgs;
-  cc = pkgs.llvmPackages_21.clang;
+  cc = pkgs.llvmPackages_22.clang;
   extra = [
     # Copied from rules_cc: https://github.com/bazelbuild/rules_cc/blob/273b7d7c4933c372fdd4f6061da6d871d0e561e9/cc/private/toolchain/clang_deps_scanner_wrapper.sh.tpl
     (pkgs.writeShellScriptBin "cc-scan-deps-wrapper" ''
@@ -11,7 +11,7 @@ import ./cc.nix rec {
       ${pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
         export SDKROOT="${pkgs.apple-sdk.sdkroot}"
       ''}
-      ${pkgs.llvmPackages_21.clang-tools}/bin/clang-scan-deps -format=p1689 -- ${cc}/bin/clang "$@" > "$DEPS_SCANNER_OUTPUT_FILE"
+      ${pkgs.llvmPackages_22.clang-tools}/bin/clang-scan-deps -format=p1689 -- ${cc}/bin/clang "$@" > "$DEPS_SCANNER_OUTPUT_FILE"
     '')
   ];
 }
